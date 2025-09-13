53f6046d2e067ec854171644a344d6cc9b7ba100
Maxim Ugrin <maksim.ugrin@wilix.team>
2025-09-13 03:00:10 +0300

    refactoring for new linter-rules

--- cmd/grpc-client/main.go
+++ cmd/grpc-client/main.go
@@ -3,70 +3,94 @@ package main
 
 import (
 	"context"
+	"errors"
 	"log"
+	"net"
 	"time"
 
+	"github.com/based-chat/chat-server/utilites/mathx"
 	"github.com/brianvoe/gofakeit/v7"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-
-	"github.com/based-chat/chat-server/utilites"
 
 	srv "github.com/based-chat/chat-server/pkg/chat/v1"
 )
 
 const (
-	grpcPort       = ":50051"
-	host           = "0.0.0.0"
-	maxTimeout     = 3 * time.Second
+	grpcPort       = "50052"
+	grpcHost       = "localhost"
+	maxTimeout     = 1 * time.Second
 	fakeCountUsers = 5
+)
+
+var (
+	errFailedConnect         = errors.New("failed to connect: %v")
+	errFailedCloseConnection = "failed to close connection: %v"
+	errFailedCreateChat      = "failed to create chat: %v"
+	errFailedSendMessage     = "failed to send message: %v"
+	errFailedDeleteChat      = "failed to delete chat: %v"
 )
 
 // main demonstrates basic usage of the chat service rpc api.
 // It connects to the server, creates a chat with a few users,
 // sends a message, and then deletes the chat.
 func main() {
-	conn, err := grpc.NewClient(host+grpcPort, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	// Connect to the grpc server
+	addr := net.JoinHostPort(grpcHost, grpcPort)
+
+	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
-		log.Fatalf("failed to connect: %v", err)
+		log.Default().Printf(errFailedConnect.Error(), err)
+		return
 	}
+
 	defer func() {
 		connErr := conn.Close()
 		if connErr != nil {
-			log.Default().Println("failed to close connection: " + connErr.Error())
+			log.Default().Printf(errFailedCloseConnection, connErr)
 		}
 	}()
 
 	c := srv.NewChatV1Client(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), maxTimeout)
-	defer cancel()
 
 	usernames := make([]string, fakeCountUsers)
 	for i := range usernames {
 		usernames[i] = gofakeit.Username()
 	}
 
-	createResponse, err := c.Create(ctx, &srv.CreateRequest{Usernames: usernames})
+	ctxCreateResponse, cancelCreateResponse := context.WithTimeout(context.Background(), maxTimeout)
+
+	createResponse, err := c.Create(ctxCreateResponse, &srv.CreateRequest{Usernames: usernames})
 	if err != nil {
-		log.Default().Println("failed to create: " + err.Error())
+		log.Default().Printf(errFailedCreateChat, err)
 		return
 	}
 
-	_, err = c.SendMessage(ctx, &srv.SendMessageRequest{
+	cancelCreateResponse()
+
+	ctxSendMessage, cancelSendMessage := context.WithTimeout(context.Background(), maxTimeout)
+
+	_, err = c.SendMessage(ctxSendMessage, &srv.SendMessageRequest{
 		ChatId:  createResponse.GetId(),
 		Sender:  gofakeit.Username(),
 		Message: gofakeit.Phrase(),
 	})
 	if err != nil {
-		log.Default().Println("failed to send message: " + err.Error())
+		log.Default().Printf(errFailedSendMessage, err)
 		return
 	}
 
-	_, err = c.Delete(ctx, &srv.DeleteRequest{
-		Id: utilites.Abs(createResponse.GetId()),
+	cancelSendMessage()
+
+	ctxDelete, cancelDelete := context.WithTimeout(context.Background(), maxTimeout)
+
+	_, err = c.Delete(ctxDelete, &srv.DeleteRequest{
+		Id: mathx.Abs(createResponse.GetId()),
 	})
 	if err != nil {
-		log.Default().Println("failed to delete: " + err.Error())
+		log.Default().Printf(errFailedDeleteChat, err)
 		return
 	}
+
+	cancelDelete()
 }
